d679b9aabf29fac2cfad0296fa574afedf073b41
Aurimas Balciunas <56936689+AurimasBalciunas@users.noreply.github.com>
2022-10-23 18:48:45 -0400

    changes

--- move_files.sh
+++ move_files.sh
@@ -1,11 +1,14 @@
-
 cd "/content/training/temp"
 
 #Sourcing constants
 source "/content/colab-helper/constants.sh"
 
+network_type="mobilenet_v1_ssd"
+ckpt_name="${ckpt_name_map[${network_type}]}"
+
+
 #Removing the incorrect pipeline file
 rm "${ckpt_name}/pipeline.config"
 
 #Moving remaining model files to checkpoint directory
-mv "${ckpt_name}/*" "${CKPT_DIR}"
+mv ${ckpt_name}/* "${CKPT_DIR}"
